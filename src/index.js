54f9a5172d6d91c0e4f892cadb6bebfc9f479758
Federico Vitale <fedevitale99@gmail.com>
2018-05-23 16:27:17 +0200

    cp

--- src/index.js
+++ src/index.js
@@ -10,50 +10,28 @@ import execa from 'execa';
 import fuzzy from 'fuzzy';
 import _ from 'lodash';
 import Ora from 'ora';
+import Listr from 'listr';
 
-import { checkCask, checkHomeBrew, getAppsList, getInstalledApps } from './utils'
+
+import {
+	checkCask,
+	checkHomeBrew,
+	getAppsList,
+	getInstalledApps,
+	exec,
+	filterCasks
+} from './utils'
 
 const spinner = new Ora();
 
 export default async (query, flags) => {
-	try {
-		spinner.start('Loading casks list...')
-
-		const source = await getAppsList();
-
-		spinner.stop();
-		pb('')
-
-		const { cask } = await inquirer.prompt([{
-			type: 'autocomplete',
-			name: 'cask',
-			message: 'Search for casks:',
-			when: a => source.length > 0,
-			source(answers, input) {
-				input = input || query|| '';
-				return new Promise(function(resolve) {
-					setTimeout(function() {
-						resolve(fuzzy.filter(input, source, { extract: e => e.appName }).map(e => e.original.appName))
-					}, _.random(30, 500));
-				});
-			}	
-		}])
-
-		if (cask) {
-			try {
-				spinner.start(chalk`Installing {yellow {bold ${cask}}}...`)
-
-				let { stdout } = await execa('brew', ['cask', 'install', cask])
-
-				spinner.succeed(chalk`{yellow {bold ${cask}}} installed!`)
-			} catch (error) {
-				throw error;
+	const tasks = new Listr([
+		{
+			title: 'Homebrew check',
+			task: ctx => {
+				ctx.hasBrew = true;
+				ctx.hasBrew = false;
 			}
-		} else {
-			console.log('Hello World', cask);
 		}
-	} catch (error) {
-		throw error;
-	}
+	])
 }
-
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,23 @@
+require('any-observable/register/rxjs-all'); // eslint-disable-line import/no-unassigned-import
+
 import got from 'got';
 import execa from 'execa';
 import pb from '@splash-cli/print-block';
 import chalk from 'chalk';
+import Observable from 'any-observable';
+import streamToObservable from 'stream-to-observable';
+import fuzzy from 'fuzzy';
+
+export function exec(cmd, args) {
+	// Use `Observable` support if merged https://github.com/sindresorhus/execa/pull/26
+	const cp = execa(cmd, args);
+
+	return Observable.merge(
+		streamToObservable(cp.stdout.pipe(split()), {await: cp}),
+		streamToObservable(cp.stderr.pipe(split()), {await: cp})
+	).filter(Boolean);
+};
+
 
 export const getAppsList = async (exclude = [], options = {
 	client_id: 'f021bc212e0b3345936c',
@@ -78,5 +94,16 @@ export async function checkHomeBrew() {
 				chalk `{dim {underline /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)}}"
 			}`);
 		})
+	});
+}
+
+export function filterCasks(source, input) {
+	input = input || '';
+	return new Promise(function (resolve) {
+		setTimeout(function () {
+			resolve(fuzzy.filter(input, source, {
+				extract: e => e.appName
+			}).map(e => e.original.appName))
+		}, _.random(30, 500));
 	});
 }
